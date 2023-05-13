738ea1a61496d0666b8e7a767ff017dfd7458d11
Jharding86 <james@techintrigue.com>
2023-05-13 15:24:59 -0600

    Added the ability to swap lineups at different parts of the game

---
+++ lineup.py
@@ -0,0 +1,58 @@
+from player import Player
+from typing import List
+
+class LineUp:
+    def __init__(self):
+        self.players:List[Player] = []
+
+    def addPlayer(self, player: Player):
+        self.players.append(player)
+
+    def print(self, otherLineup):
+        if(otherLineup == 0):
+            for player in self.players:
+                print(player)
+        else:
+            mPlayersLeaving     = []
+            mPlayersEntering    = []
+
+            for player in self.players:
+                if self.doesLineUpContainPlayer(otherLineup, player) == False:
+                    mPlayersEntering.append(player)
+                # print(player)
+
+            for player in otherLineup.players:
+                if otherLineup.doesLineUpContainPlayer(self, player) == False:
+                    mPlayersLeaving.append(player)
+
+            # print("\nPlayers Leaving:")
+            # for player in mPlayersLeaving:
+            #     print(player)
+
+            # print("\nPlayers Entering:")
+            # for player in mPlayersEntering:
+            #     print(player)
+            
+            print("Line Up                             Players Leaving                         Players Entering")
+            for i in range(0, len(self.players)):
+                print("{0:40}".format(self.players[i].name), end=" ")
+                if i < len(mPlayersLeaving):
+                    print("{0:40}".format(mPlayersLeaving[i].name), end=" ")
+                else:
+                    print("{0:40}".format(""), end=" ")
+                if i < len(mPlayersEntering):
+                    print("{0:40}".format(mPlayersEntering[i].name))
+                else:
+                    print("{0:40}".format(""))
+
+            #         row2 = "{0:40}{1:40}{2}".format(self.players[0], mPlayersLeaving[0], mPlayersEntering[0])
+            # row3 = "{0:40}{1:40}{2}".format(self.players[1], mPlayersLeaving[1], mPlayersEntering[1])
+            # row4 = "{0:40}{1:40}{2}".format(self.players[2], mPlayersLeaving[2], mPlayersEntering[2])
+            # row5 = "{0:40}{1:40}{2}".format(self.players[3], mPlayersLeaving[3], mPlayersEntering[3])
+
+    def doesLineUpContainPlayer(self, linueup, player:Player):
+        for p in linueup.players:
+            if p.name == player.name:
+                return True
+        
+        return False
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from player import Player
-
-def printPlayers(players):
-    for player in players:
-        print(player)
+from lineup import LineUp
+from typing import List
 
 # mPlayerNames = ["Logan", "Atticus", "Will", "Rowan", "Cooper", "Baxter", "Jackson"]
-# mPlayerNames = ["Logan", "Atticus", "Will", "Baxter", "Cooper", "Rowan", "Jackson", "Tommy"]
-mPlayerNames = ["Logan", "Atticus", "Cooper", "Rowan", "Will", "Tommy", "Jackson", "Baxter"]
+# mPlayerNames = ["Logan", "Atticus", "Will", "tBaxter", "Cooper", "Rowan", "Jackson", "Tommy"]
+mPlayerNames = ["Logan", "Atticus", "Cooper", "Tommy", "Will", "Rowan", "Jackson", "Baxter"]
 mNumPlayers = len(mPlayerNames)
-
+mLineups = []
 mPlayers = []
+
 for i in range(0, mNumPlayers):
     player = Player(mPlayerNames[i])
     mPlayers.append(player)
-
 
 mNumPeriods = 4
 mPeriodLen = 8
@@ -21,84 +19,65 @@ mSubPeriod = 4
 mNumPlayersToSub = 2
 mTotalGameTime = mNumPeriods * mPeriodLen
 
-print("Number of Players:", mNumPlayers)
-print("Total Game Time is {0} minutes.".format(mNumPeriods * mPeriodLen))
-print("Sub every {0} minutes.".format(mSubPeriod))
-print("{0} players swap at each sub interval".format(mNumPlayersToSub))
-print("")
+def createLineup(players:List[Player]):
+    nLineup = LineUp()
+    for player in players:
+        player.incrementPlays(1)
+        nLineup.addPlayer(player)
 
-mStartPlayers = []
-for i in range(0,4):
-    mStartPlayers.append(mPlayers.pop())
+    mLineups.append(nLineup)
 
-print("Starting Line Up")
-for k in range(0,4):
-    mStartPlayers[k].incrementPlays(1)
-printPlayers(mStartPlayers)
-print()
+def generateLineups():
+    mStartPlayers = []
+    for i in range(0,4):
+        mStartPlayers.append(mPlayers.pop())
 
-for i in range(0, int(mTotalGameTime / mSubPeriod)-1):
-    playersLeaving = []
-    playersEntering = []
-    for j in range(0, mNumPlayersToSub):
-        playersLeaving.append(mStartPlayers.pop(0))
-        playersEntering.append(mPlayers.pop(0))
+    createLineup(mStartPlayers)
 
-        mStartPlayers.append(playersEntering[j])
-        mPlayers.append(playersLeaving[j])
+    for i in range(0, int(mTotalGameTime / mSubPeriod)-1):
+        playersLeaving = []
+        playersEntering = []
+        for j in range(0, mNumPlayersToSub):
+            playersLeaving.append(mStartPlayers.pop(0))
+            playersEntering.append(mPlayers.pop(0))
 
-    # mPlayers.append(mStartPlayers.pop(0))
-    # mPlayers.append(mStartPlayers.pop(0))
-    # mStartPlayers.append(mPlayers.pop(0))
-    # mStartPlayers.append(mPlayers.pop(0))
+            mStartPlayers.append(playersEntering[j])
+            mPlayers.append(playersLeaving[j])
 
-    for k in range(0,4):
-        mStartPlayers[k].incrementPlays(1)
+        createLineup(mStartPlayers)
 
-    p1 = str(mStartPlayers[0])
-    p2 = str(mStartPlayers[1])
-    p3 = str(mStartPlayers[2])
-    p4 = str(mStartPlayers[3])
+def printLineups():
+    print("Number of Players:", mNumPlayers)
+    print("Total Game Time is {0} minutes.".format(mNumPeriods * mPeriodLen))
+    print("Sub every {0} minutes.".format(mSubPeriod))
+    print("{0} players swap at each sub interval".format(mNumPlayersToSub))
+    print("")
 
-    l1 = str(playersLeaving[0])
-    l2 = str(playersLeaving[1])
+    for i in range(0, len(mLineups)):
+        print("\nLineup {0}".format(i))
+        if(i != 0):
+            mLineups[i].print(mLineups[i-1])
+        else:
+            mLineups[i].print(0)
 
-    e1 = str(playersEntering[0])
-    e2 = str(playersEntering[1])
+def shiftLineupDown():
+    print("Enter the lineup index to shift down:")
+    uInput = input()
 
-    row0 = "Substitution {0}".format(i)
-    row1 = "Line Up                             Players Leaving                         Players Entering".format(i)
-    row2 = "{0:40}{1:40}{2}".format(p1, l1, e1)
-    row3 = "{0:40}{1:40}{2}".format(p2, l2, e2)
-    row4 = "{0}".format(p3)
-    row5 = "{0}".format(p4)
-    
-    print(row0)
-    print(row1)
-    print(row2)
-    print(row3)
-    print(row4)
-    print(row5)
-    print()
+    lineup = mLineups.pop(int(uInput))
+    mLineups.insert(int(uInput)+1, lineup)
+    printLineups()
+    selectAction()
 
-    # print("Substition: ", i)
-    # print("Players Leaving:")
-    # printPlayers(playersLeaving)
-    # print("")
-    # print("Players Entering:")
-    # printPlayers(playersEntering)
-    # print("")
-    # print("Current Line up:")
-    # printPlayers(mStartPlayers)
-    # print("")
+def selectAction():
+    print("\nMake a selection:\n 1. Shift Lineup Down\n 2. Exit")
 
+    uInput = input()
 
+    if int(uInput) == 1:
+        shiftLineupDown()
 
+generateLineups()
+printLineups()
+selectAction()
 
-#Add all players to one array
-for i in range(0, len(mPlayers)):
-    mStartPlayers.append(mPlayers.pop())
-
-#Print all players stats
-for i in range(0, mNumPlayers):
-    print(mStartPlayers[i])
